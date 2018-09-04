1ef836d9eece034ec1bfa06581e58ee586afd2bd
huynhhien246 <huynhhien246@gmail.com>
2018-09-04 10:44:58 +0700

    Update file Front-End_OnlineQuiz4

--- Front-End/OnlineQuiz_Front-End4/js/custom.js
+++ Front-End/OnlineQuiz_Front-End4/js/custom.js
@@ -16,52 +16,24 @@ function checkTime(i) {
     return i;
 }
 
-$(window).scroll(function() {
-    if ($(".navbar").offset().top > 70) {
-        $(".top-area").addClass("top-padding");
-
-    } else {
-        $(".top-area").removeClass("top-padding");
-    }
-});
-
 (function($) {
-    "use strict"; // Start of use strict
+    $(window).scroll(function() {
+        if ($(".navbar").offset().top > 70) {
+            $(".top-area").addClass("top-padding");
+            $(".sidebar").addClass("top-padding");
+        } else {
+            $(".top-area").removeClass("top-padding");
+            $(".sidebar").removeClass("top-padding");
+        }
+    });
+
 
     // Toggle the side navigation
     $("#sidebarToggle").click(function(e) {
         e.preventDefault();
         $("body").toggleClass("sidebar-toggled");
         $(".sidebar").toggleClass("toggled");
+        $("#wrapper #content-wrapper").toggleClass("left-margin");
     });
 
-    // Prevent the content wrapper from scrolling when the fixed side navigation hovered over
-    $('body.fixed-nav .sidebar').on('mousewheel DOMMouseScroll wheel', function(e) {
-        if ($window.width() > 768) {
-            var e0 = e.originalEvent,
-                delta = e0.wheelDelta || -e0.detail;
-            this.scrollTop += (delta < 0 ? 1 : -1) * 30;
-            e.preventDefault();
-        }
-    });
-
-    // Scroll to top button appear
-    $(document).scroll(function() {
-        var scrollDistance = $(this).scrollTop();
-        if (scrollDistance > 100) {
-            $('.scroll-to-top').fadeIn();
-        } else {
-            $('.scroll-to-top').fadeOut();
-        }
-    });
-
-    // Smooth scrolling using jQuery easing
-    $(document).on('click', 'a.scroll-to-top', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: ($($anchor.attr('href')).offset().top)
-        }, 1000, 'easeInOutExpo');
-        event.preventDefault();
-    });
-
-})(jQuery); // End of use strict
+})(jQuery);

0b49ba432c6f4ae50747739cc5e9164997e763cd
Tienmanh12 <40880600+Tienmanh12@users.noreply.github.com>
2018-09-05 16:17:33 +0700

    Revert "importExcel"
    This reverts commit f2cb58baa45e6c9ee11ae1492ff09e894a4ad1d9.

--- Back-End/Project_OnlineQuiz/Project_OnlineQuiz/Controllers/addImportExcController.cs
+++ Back-End/Project_OnlineQuiz/Project_OnlineQuiz/Controllers/addImportExcController.cs
@@ -14,9 +14,8 @@ namespace Project_OnlineQuiz.Controllers
     public class addImportExcController : Controller
     {
         // GET: addImportExc
-        //tạo chuỗi connect string với database 
+        //tạo chuỗi connect
         SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["DB_OnlineQuizEntities"].ConnectionString);
-
         OleDbConnection Econ;
         public ActionResult Index()
         {
@@ -32,18 +31,15 @@ namespace Project_OnlineQuiz.Controllers
             insertExceldata(filepath,filename);
                 return View();
         }
-
-        //chuỗi kết nối excel với file .xlsx
         private void ExcelConn(string filepath)
         {
-            string constr = string.Format("Provider = Microsoft.ACE.OLEDB.12.0; Data Source = {0}; Extended Properties = 'Excel 8.0;HDR=YES'", filepath);
+            string constr = string.Format(@"Provider=Microsoft.ACE.OLEDB.16.0;Data Source={0};Extended Properties=""Excel 16.0 xml;HDR=YES;""", filepath);
             Econ = new OleDbConnection(constr);
         }
         private void insertExceldata( string filepath,string filename)
         {
             string fullpath = Server.MapPath("/Excelfolder/") + filename;
             ExcelConn(fullpath);
-            //sheetname
             string query = string.Format("Select * from [{0}]", "Sheet1$");
             OleDbCommand Ecom = new OleDbCommand(query, Econ);
             Econ.Open();
@@ -54,19 +50,22 @@ namespace Project_OnlineQuiz.Controllers
 
             DataTable dt = ds.Tables[0];
             SqlBulkCopy objbulk = new SqlBulkCopy(con);
-            //Give your Destination table name
             objbulk.DestinationTableName = "Questions";
+<<<<<<< HEAD
             
             //Mappings
             objbulk.ColumnMappings.Add("CodeQuestion", "CodeQuestion");
+=======
+          //  objbulk.ColumnMappings.Add("CodeQuestion", "CodeQuestion");
+>>>>>>> parent of f2cb58b... importExcel
             objbulk.ColumnMappings.Add("Text", "Text");
             objbulk.ColumnMappings.Add("Image", "Image");
             objbulk.ColumnMappings.Add("Type", "Type");
             objbulk.ColumnMappings.Add("Difficulty", "Difficulty");
-            objbulk.ColumnMappings.Add("NumOfIterations", "NumOfIterations");
+            objbulk.ColumnMappings.Add("NumOflterations", "NumOflterations");
             objbulk.ColumnMappings.Add("CodeSubject", "CodeSubject");
             objbulk.ColumnMappings.Add("CodeTeacher", "CodeTeacher");
-            objbulk.ColumnMappings.Add("CodeGeneralQuestion", "CodeGeneralQuestion");
+            objbulk.ColumnMappings.Add("CodeGeneralQuetion", "CodeGeneralQuetion");
             objbulk.ColumnMappings.Add("CodeChapter", "CodeChapter");
             con.Open();
             objbulk.WriteToServer(dt);
